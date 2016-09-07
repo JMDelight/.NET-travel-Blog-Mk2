d81e1ef183b13b4e59f165a9d48b8002819857ac
Ben Logue and Carl Egbert <ben+carl@>
2016-09-07 09:27:02 -0700

    set up project.json, startup.cs, homecontroller, dbcontext, database migration

---
+++ TravelBlog/src/TravelBlog/Migrations/20160907161737_Initial.Designer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TravelBlog.Models;
+
+namespace TravelBlog.Migrations
+{
+    [DbContext(typeof(TravelBlogDbContext))]
+    [Migration("20160907161737_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rc2-20901")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("TravelBlog.Models.Experience", b =>
+                {
+                    b.Property<int>("ExperienceId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<int>("LocationId");
+
+                    b.HasKey("ExperienceId");
+
+                    b.HasIndex("LocationId");
+
+                    b.ToTable("Experiences");
+                });
+
+            modelBuilder.Entity("TravelBlog.Models.Location", b =>
+                {
+                    b.Property<int>("LocationId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("LocationId");
+
+                    b.ToTable("Locations");
+                });
+
+            modelBuilder.Entity("TravelBlog.Models.Experience", b =>
+                {
+                    b.HasOne("TravelBlog.Models.Location")
+                        .WithMany()
+                        .HasForeignKey("LocationId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
